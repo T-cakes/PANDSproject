fafba351d347b148089ba36b51450ed144fc6761
Stephen Caulfield <g00398240@gmit.ie>
2021-04-26 23:34:35 +0100

    Change graph method, Compacted code into functions

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # PANDSproject
-Analysis of FIshers Iris Data using Python
+
+---
+
+# Analysis of FIshers Iris Data using Python
+
+The Fisher's Iris Data set was first founded by the British Statitician, geneticist and academic: Ronald Aylmer Fisher.
+In 1936 he wrote an article titled "The Use of Multiple Measurements in Taxonomic Problems" in a journal called "Annals of Eugenics
+In this article, he developed a linear function to differentiate various Iris species based on their Petal and Sepal lengths and widths.
+---
+
+# References
+[1]The Iris Dataset — A Little Bit of History and Biology - https://towardsdatascience.com/the-iris-dataset-a-little-bit-of-history-and-biology-fb4812f5a7b5
+
+[2]Annals of Human Genetics - https://onlinelibrary.wiley.com/doi/epdf/10.1111/j.1469-1809.1936.tb02137.x
--- analysis.py
+++ analysis.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sb
 
 #Removes Limit of lines in Dataframe
 pd.set_option("display.max_rows", None, "display.max_columns", None)
@@ -14,49 +15,33 @@ f = open('output.txt', 'w')
 f.write(str(data.groupby(['Name']).describe()))
 f.close()
 
-
-plt.figure(1)
-data['Sepal Length'].hist(bins = [0,1,2,3,4,5,6,7], color = 'red')
-plt.title('Sepal Length')
-plt.xlabel('sepal length in cm')
-plt.ylabel('Amount')
-plt.savefig('histogram/SepalLength.png')
-
-plt.figure(2)
-data['Sepal Width'].hist(bins = [0,1,2,3,4,5,6,7], color = 'orange')
-plt.title('Sepal Width')
-plt.xlabel('sepal width in cm')
-plt.ylabel('Amount')
-plt.savefig('histogram/SepalWidth.png')
-
-plt.figure(3)
-data['Petal Length'].hist(bins = [0,1,2,3,4,5,6,7])
-plt.title('Petal Length')
-plt.legend(['petal', 'sepal'])
-plt.xlabel('petal length in cm')
-plt.ylabel('Amount')
-plt.savefig('histogram/PetalLength.png')
-
-plt.figure(4)
-data['Petal Width'].hist(bins = [0,1,2,3,4,5,6,7], color = 'indigo')
-plt.title('Petal Width')
-plt.xlabel('petal width in cm')
-plt.ylabel('Amount')
-plt.savefig('histogram/PetalWidth.png')
-
-colors = {'Iris-setosa':'red', 'Iris-versicolor':'blue', 'Iris-virginica':'green'}
-
-
-#https://kanoki.org/2020/08/30/matplotlib-scatter-plot-color-by-category-in-python/
-plt.figure(5)
-fig, ax = plt.subplots()
 grouped = data.groupby('Name')
-for key, group in grouped:
-    group.plot(ax=ax,kind='scatter', x = 'Sepal Length', y = 'Sepal Width',label=key, color = colors[key])
-plt.savefig('ScatterPlots/SepalScatter.png')
+colors = {'Iris-setosa':'red', 'Iris-versicolor':'orange', 'Iris-virginica':'purple'}
 
-plt.figure(6)
-for key, group in grouped:
-    group.plot(ax=ax,kind='scatter', x = 'Petal Length', y = 'Petal Width', color = colors[key])
-plt.savefig('ScatterPlots/PetalScatter.png')
-plt.show()
+def plothist(dim):
+    fig, ax = plt.subplots()
+    for key, group in grouped:
+        #https://www.dataindependent.com/pandas/pandas-histogram/
+        group[dim].plot(ax=ax, kind='hist', alpha = 0.4, title = dim, bins = 10, grid=True,  color = colors[key])
+    plt.legend(['Iris-setosa', 'iris-versicolor', 'iris-virginica'])
+    plt.xlabel( dim + ' in cm')
+    plt.ylabel('Amount')
+    plt.savefig('histogram/'+ dim + '.png')
+    plt.show()
+    plt.close()
+
+def scatplot(mes):
+    #https://kanoki.org/2020/08/30/matplotlib-scatter-plot-color-by-category-in-python/
+    fig, ax = plt.subplots()
+    for key, group in grouped:
+        group.plot(ax=ax,kind='scatter', x = mes + ' Length', y = mes + ' Width', grid = True, label=key, color = colors[key])
+    plt.savefig('ScatterPlots/'+ mes +' Scatter.png')
+    plt.show()
+    plt.close()
+
+plothist('Sepal Length')
+plothist('Sepal Width')
+plothist('Petal Length')
+plothist('Petal Width')
+scatplot('Sepal')
+scatplot('Petal')
